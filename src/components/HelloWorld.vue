e42d04ccdefc621c160beb9fc7bfa785a25b64ef
Volodymyr Kryvonos <volodymyr.kryvonos@redvike.com>
2018-05-30 18:29:14 +0200

    Initial commit.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <Page class="page">
+    <ActionBar class="action-bar" title="Hello world"></ActionBar>
+
+    <StackLayout class="hello-world">
+      <Label class="body" textWrap=true text="This is a hello world component, tap the button if you dare"/>
+
+      <Button class="btn btn-primary" @tap="surprise = !surprise" text="Tap me!"/>
+  </Page>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        surprise: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .logo-container {
+    /* background: gold; */
+  }
+
+  .logo {
+    margin-top: -30;
+  }
+
+  .hello-world {
+    margin: 20;
+  }
+
+  Label {
+    color: red;
+  }
+</style>
